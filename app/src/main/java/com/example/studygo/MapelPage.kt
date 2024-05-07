71e2a0d843d3418b1231b20200f00b5646a6b818
Azzahraa1 <fatimahazzahra4121@gmail.com>
2024-05-07 15:55:30 +0700

    layout

---
+++ app/src/main/java/com/example/studygo/MapelPage.kt
@@ -0,0 +1,11 @@
+package com.example.studygo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MapelPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mapel_page)
+    }
+}
